d190d7f6fc9790756c4e2cd072aeb5eee9eda1d9
PIYUSH SHARMA <piyushsharma6715@gmail.com>
2018-05-23 00:37:09 +0530

    Create Child.java

---
+++ Inheritance/Child.java
@@ -0,0 +1,22 @@
+class Parent
+{
+	int  num=10;
+	void func()
+	{
+		System.out.println("Parent:func");
+	}
+}
+class Child extends Parent
+{
+	int  num=20;
+	void func()
+	{
+		System.out.println("Child:func");
+	}
+	public static void main(String a[])
+	{
+		Parent p=new Child();
+		Child c=(Child)p;
+		System.out.println("Value of parent num is "+p.num+" Value of Child num is "+c.num);
+	}
+}
